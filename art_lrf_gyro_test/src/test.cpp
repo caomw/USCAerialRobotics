e11d14c59974dcfafff2adf3206b16ee9a84a959
beohawk <art+beohawk@uscrs.org>
2011-10-31 15:57:52 -0700

    Update art_lrf_gyro_test/src/test.cpp

--- art_lrf_gyro_test/src/test.cpp
+++ art_lrf_gyro_test/src/test.cpp
@@ -1,11 +1,18 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
-#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/Vector3Stamped.h>
 
 using namespace std;
 
+ros::Publisher pub;
+
 void hahacallback (const geometry_msgs::Vector3::ConstPtr& msg)
 {
+  geometry_msgs::Vector3Stamped vec;
+  vec.vector = *msg;
+  vector.header.stamp = ros::Time::now();
+  pub.publish(vec);
+  
   static tf::TransformBroadcaster br;
   tf::Transform trafo;
   trafo.setOrigin(tf::Vector3(0,0,0));
@@ -17,6 +24,7 @@ int main (int argc, char** argv)
 {
   ros::init(argc, argv, "abcdefg");
   ros::NodeHandle nh;
+  pub = nh.publish<geometry_msgs::Vector3Stamped>("/angle", 10);
   ros::Subscriber sub = nh.subscribe("/haha", 10, hahacallback);
   ros::spin();
   return 0;
