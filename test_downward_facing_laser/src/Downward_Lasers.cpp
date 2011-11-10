360e3b7b72f565bc71f0dddfb3fa9a20f991d2f5
Aerial Robotics <art+beohawk@uscrs.org>
2011-11-09 20:10:53 -0800

    added code for identifying circles

--- test_downward_facing_laser/src/Downward_Lasers.cpp
+++ test_downward_facing_laser/src/Downward_Lasers.cpp
@@ -1,6 +1,8 @@
 
 #include <ros/ros.h>
 #include <cmath>
+#include <iostream>
+#include <cv.h>
 #include <sensor_msgs/Image.h>
 #include <cv_bridge/cv_bridge.h>
 #include <boost/foreach.hpp>
@@ -9,7 +11,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include "opencv2/calib3d/calib3d.hpp"
+
 
 using namespace std;
 
@@ -19,17 +21,24 @@ using cv::Mat;
 
 class Downward_Lasers
 {	
+  public:
 	ros::NodeHandle nh;
 	ros::Subscriber sub_image;
 	uint sub_image_count;
-	Mat img_color, img_gray;
+	Mat img_color;
+	Mat img_gray;
 	vector<cv::Vec3f> circles;
 	
 	
   public:
 
-	Downward_Lasers(ros::NodeHandle& _nh): nh(_nh), sub_image_count(0), img_color(cv::Size(640, 480), CV_8UC3), img_gray(cv::Size(640, 480), CV_8UC3)
+	Downward_Lasers(ros::NodeHandle& _nh)//: nh(_nh), sub_image_count(0), img_color(cv::Size(640, 480), cv::CV_8UC3), img_gray(cv::Size(640, 480), cv::CV_8UC3)
 	{
+		nh = _nh;
+		img_color = Mat(cv::Size(640, 480), CV_8UC3);
+		img_gray = Mat(cv::Size(640, 480), CV_8UC3);
+
+
 		sub_image = nh.subscribe("/usb_cam/image_raw", 10, &Downward_Lasers::sub_image_callback, this);		
 		cv::namedWindow("test_result");
 	}
@@ -46,70 +55,28 @@ class Downward_Lasers
 		if(sub_image_count % 10 == 0)
 		{
 			convert_image(msg, img_color);
+			
+			//TODO- filter the image
 
 			//convert to greyscale and smooth to avoid multiple flase circles in close proximity
-      cv::cvtColor(img_color, img_gray, CV_BGR2GREY);
-      cv::GaussianBlur( img_gray, img_gray, cv::Size(9, 9), 2, 2);
+			cv::cvtColor(img_color, img_gray, CV_BGR2GRAY);
+			cv::GaussianBlur( img_gray, img_gray, cv::Size(9, 9), 2, 2);
 
 			//get circles
-      cv::HoughCircles(img_gray, circles, CV_HOUGH_GRADIENT, 2, img_grey->rows/4, 100, 100, 0, 4 );
-			
-			if(sub_image_count == 10)
+			cv::HoughCircles(img_gray, circles, CV_HOUGH_GRADIENT, 2, img_gray.rows/4, 100, 100, 0, 4 ); 
+
+			//findHomography(Mat(points_matches_src), Mat(points_matches_tgt), mask_outliers, CV_RANSAC, 3);
+				
+			//Draw circles on screen
+			for(int i = 0; i < circles.size(); i++)
 			{
-				
-			}
-			else if(sub_image_count == 20)
-			{	
-				Mat roi_1(img_result, cv::Rect(0, 0, 640, 480));
-				img_src.copyTo(roi_1);
-				Mat roi_2(img_result, cv::Rect(0, 480, 640, 480));
-				img_tgt.copyTo(roi_2);
-				roi_1.release();
-				roi_2.release();
-				
-				descriptor_matcher.match(descriptors_src, descriptors_tgt, match_result);
-				
-				// TODO: Cross Matching check
-				
-				vector<int> indices_matches_src(match_result.size()), indices_matches_tgt(match_result.size());
-			    for(uint32_t i = 0; i < match_result.size(); i++)
-			    {
-			        indices_matches_src[i] = match_result[i].queryIdx;
-			        indices_matches_tgt[i] = match_result[i].trainIdx;
-			    }
-			    
-			    // convert vector of keypoints to vector of points
-			    vector<cv::Point2f> points_matches_src, points_matches_tgt;
-			    cv::KeyPoint::convert(*keypoints_src_ptr, points_matches_src, indices_matches_src);
-			    cv::KeyPoint::convert(*keypoints_tgt_ptr, points_matches_tgt, indices_matches_tgt);
-			    
-			    vector<uchar> mask_outliers;
-			    findHomography(Mat(points_matches_src), Mat(points_matches_tgt), mask_outliers, CV_RANSAC, 3);
-				
-				cout << endl;
-				
-				for(int i = 0; i < mask_outliers.size(); i++)
-				{
-					cout << ((int) mask_outliers[i]) << " ";
-					if(mask_outliers[i] == 1)
-					{
-						points_matches_tgt[i].y += 480;
-					    cv::line(img_result, points_matches_src[i] , points_matches_tgt[i], cv::Scalar(255,0,0));
-					    points_matches_tgt[i].y -= 480;
-					}
-				}				
-				
-				cout << endl;
-				
-				cv::imshow("test_result", img_result);
-				cv::waitKey(0);
-			}
-			
-			img_src = img_tgt;
-			keypoints_src_ptr = keypoints_tgt_ptr;
-			descriptors_src = descriptors_tgt;
-			match_result.clear();
+				cv::circle(img_color, cv::Point2f(circles[i][0], circles[i][1]), circles[i][3], cv::Scalar(255,0,0), 1, 8, 0);
+			}				
+
+			cv::imshow("test_result", img_color);
+			//cv::waitKey(0);
 		}
+		cout << sub_image_count << endl;
 		++ sub_image_count;
 	}
 
@@ -125,10 +92,10 @@ class Downward_Lasers
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "test_feature_matching");
+	ros::init(argc, argv, "test_downward_facing_lasers");
 	ros::NodeHandle nh;
 	
-	Feature_Matching feature_matching(nh);
+	Downward_Lasers downward_lasers(nh);
 	
 	ros::spin();
 	return 0;
