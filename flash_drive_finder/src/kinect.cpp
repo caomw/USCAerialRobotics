71483a34a93a59413e1cc3f2309bc99e902b1c36
Russell Stewart <russell@russell-laptop.(none)>
2013-02-01 19:29:25 -0800

    added fovis dependencies and included bag file

--- flash_drive_finder/src/kinect.cpp
+++ flash_drive_finder/src/kinect.cpp
@@ -11,9 +11,12 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
-
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/image_encodings.h>
+
+#include <fovis/fovis.hpp>
+#include "data_capture.hpp"
+
 
 namespace enc = sensor_msgs::image_encodings;
 
