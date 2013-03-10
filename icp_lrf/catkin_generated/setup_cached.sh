c3b2328313f304611636b60744c758a8ac168180
Russell Stewart <russell@russell.(none)>
2013-03-10 14:57:10 -0700

    added the icp_lrf package

---
+++ icp_lrf/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/russell/ros/USCAerialRobotics/icp_lrf/test_results"
+export ROS_TEST_RESULTS_DIR="/home/russell/ros/USCAerialRobotics/icp_lrf/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/russell/ros/USCAerialRobotics/icp_lrf/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/russell/ros/USCAerialRobotics/icp_lrf/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/russell/ros/USCAerialRobotics/icp_lrf/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/russell/ros/USCAerialRobotics/icp_lrf/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/russell/ros/USCAerialRobotics/icp_lrf/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/russell/ros/USCAerialRobotics/icp_lrf/devel/lib/python2.7/dist-packages:/opt/ros/groovy/lib/python2.7/dist-packages:/home/russell/Templates:/home/russell/git/basics/"
+export ROS_PACKAGE_PATH="/home/russell/ros/USCAerialRobotics/icp_lrf:/opt/ros/groovy/share:/opt/ros/groovy/stacks"
---
+++ icp_lrf/devel/setup.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+# generated from catkin/cmake/templates/setup.zsh.in
+
+CATKIN_SHELL=zsh
+emulate sh # emulate POSIX
+. "/home/russell/ros/USCAerialRobotics/icp_lrf/devel/setup.sh"
+emulate zsh # back to zsh mode
